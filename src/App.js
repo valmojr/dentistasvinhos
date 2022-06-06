21b6a8fce5f73e814c064bacf53376276e18da5c
Valmo <val_mo_jr@hotmail.com>
2022-06-06 00:04:55 -0300

    atualizado display

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ function App() {
     <div className="gallery">
       <div className="thumbnail">
         <img src={alana} alt="" className='faces'/>
-        <h1 className="stats">ALANA PEREIRA</h1>
+        <h1 className="stats">Alana Pereira</h1>
         <h4>Prof. Dra.</h4>
         <p>Facetas, Resina e Cerâmica</p>
       </div>
@@ -49,8 +49,8 @@ function App() {
         <h4>Professor Titular da UFSC</h4>
         <p>Restaurações de Dentes anteriores</p>
       </div>
-      <div className="thumbnail">
-        <img src={chaim} alt="" className='faces'/>
+      <div className="thumbnail_main">
+        <img src={chaim} alt="" className='faces_main'/>
         <h1 className="stats">Marcelo Chaim</h1>
         <h4>Professor Titular da UFSC</h4>
         <p>Desenho do Sorriso</p>
@@ -68,8 +68,8 @@ function App() {
       </article>
       <article className="footer_column">
         <h3>LOCALIZAÇÃO</h3>
-        <img src={serragaucha} alt="" className="cards"/>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla </p>
+        <iframe className='maps' src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d315333.9778843149!2d-51.8549366597133!3d-29.165227495526832!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x951c392c8848eb97%3A0x20dbb6297dd57a9d!2sVin%C3%ADcola%20Vinhedos%20Capoani%20-%20Vinhos%20e%20Espumantes!5e0!3m2!1spt-BR!2sbr!4v1654481993637!5m2!1spt-BR!2sbr" width="80%" height="300px" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        <p>A vinícola Capoani está localizada na região da Serra Gaúcha, mais precisamente no Vale dos Vinhedos, onde contribui com o enoturismo de Bento Gonçalves e região, com atendimento de excelência e experiências memoráveis no mundo dos vinhos e espumantes.</p>
       </article>
     </footer>
     <section className="footer_banner" id="contact">
@@ -77,7 +77,7 @@ function App() {
       <p className="hero_header">NÃO PERCA!</p>
       <div className="button">INSCREVER-SE</div>
     </section>
-    <div className="copyright">&copy;2022- <strong>Feito por Valmo Trindade</strong></div>
+    <div className="copyright">&copy;2022- <strong>Feito por <a href='https://github.com/valmojr' target="_blank">Valmo Trindade</a></strong></div>
   </div>
   );
 }
